19f84e6a3b6816696d7e2b68dca957b3127124e0
GustavoCR021 <gustavoc1922@gmail.com>
2024-04-17 22:23:55 -0300

    feat: start

--- tes/src/app/page.tsx
+++ tes/src/app/page.tsx
@@ -1,13 +1,11 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+
 import Header from "../../components/Header";
-// import InputField from "../../components/InputField";
 
 export default function Home() {
   return (
     <main> 
       <Header/>
-      {/* <InputField label={"Username"}/> */}
     </main>
   );
 }
