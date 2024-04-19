1f6bdea84cad8f46ce1a13dfccb9ca5f00ffff1f
GustavoCR021 <gustavoc1922@gmail.com>
2024-04-19 00:00:18 -0300

    feat: nav styled

--- tes/src/app/components/Nav/index.tsx
+++ tes/src/app/components/Nav/index.tsx
@@ -1,8 +1,27 @@
 import { NaV } from "./styles";
 
+import { RiMenuFill } from "react-icons/ri";
+
 
 export default function Nav() {
   return (
-    <NaV/>
+    <NaV>
+      <div className="container">
+        <div className="nav">
+          <RiMenuFill /><button>Compre por departamentos</button>
+          <div className="a">
+            <a href="/">Telefonia</a>
+            <a href="/">Eletrodomésticos</a>
+            <a href="/">TVs e Vídeo</a>
+            <a href="/">Móveis</a>
+            <a href="/">Eletroportáveis</a>
+            <a href="/">Informática</a>
+            <a href="/">Serviços</a>
+            <a href="/">Carnê Digital</a>
+          </div>
+        </div>
+      </div>
+      <hr />
+    </NaV>
   )
 }
--- tes/src/app/components/Nav/styles.ts
+++ tes/src/app/components/Nav/styles.ts
@@ -1,7 +1,40 @@
 import styled from "styled-components";
 
 export const NaV = styled.div`
-  width: 500px;
-  height: 200px;
-  background: yellow;
+  .container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100vw;
+    height: 48px;
+    background: #0033c6;
+  }
+  .nav {
+    color: #fff;
+    display: flex;
+    align-items: center;
+    /* justify-content: space-between; */
+
+    button {
+    margin-right: 45px;
+      border: none;
+      background-color: transparent;
+      color: #fff;
+      
+    }
+    .a {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      gap: 55px;
+      a {
+        color: #fff;
+        
+      }
+    }
+  }
+  hr {
+    border: 2px solid;
+    color: red;
+  }
 `
